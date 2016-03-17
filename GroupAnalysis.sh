deed305b338493295faa607760a6cd2dc5031112
ashtonteng <ashtonteng@gmail.com>
2016-03-17 15:06:20 -0700

    Update GroupAnalysis.sh

--- GroupAnalysis.sh
+++ GroupAnalysis.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-stringCommand="3dttest -base1 0 -prefix t-test -set2 "
+###GROUP ANALYSIS###
+stringCommand="3dttest -base1 0 -prefix t-test -set2 " #performs t-test on the follow set of data:
 for subject in /home/despoB/connectome-thalamus/Thalamus_ROI_connectivity/headbrik_intralaminar/*; do
-	stringCommand="${stringCommand}${subject}/stats+orig "
+	stringCommand="${stringCommand}${subject}/stats+orig " #add each subject's files into the command to form set
 done
-$stringCommand
+$stringCommand #runs the combined command
